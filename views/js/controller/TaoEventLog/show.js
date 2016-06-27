783e29c730a651c230eadff09f8bf3cd462ec995
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-06-27 19:30:13 +0300

    GUI for EventLog

--- views/js/controller/TaoEventLog/show.js
+++ views/js/controller/TaoEventLog/show.js
@@ -20,9 +20,17 @@
 
 define([
     'jquery',
-    'i18n'
-], function ($, __) {
+    'i18n',
+    'helpers',
+    'ui/datatable',
+    'tpl!taoEventLog/controller/TaoEventLog/show/layout',
+    'jqueryui',
+    'jquery.timePicker'
+], function ($, __, helpers, datatable, layoutTpl) {
     'use strict';
+
+    //the endpoints
+    var listUrl = helpers._url('search', 'TaoEventLog', 'taoEventLog');
 
     return {
 
@@ -30,7 +38,90 @@ define([
          * Controller entry point
          */
         start: function start() {
-            console.log('here');
+
+            var $layout = $(layoutTpl());
+            var $eventList = $('.log-browser .log-table', $layout);
+            var $eventViewer = $('.event-viewer', $layout);
+            
+            // timePickers
+            var $fromDate = $('.log-browser .from-time', $layout);
+            $fromDate.datetimepicker({
+                onClose: function( selectedDate ) {
+                    $( ".log-browser .to-time" ).datepicker( "option", "minDate", selectedDate );
+                }
+            });
+
+            var $toDate = $('.log-browser .to-time', $layout);
+            $toDate.datetimepicker({
+                onClose: function( selectedDate ) {
+                    $( ".log-browser .from-time" ).datepicker( "option", "maxDate", selectedDate );
+                }
+            });
+            
+            var updateEventDetails = function updateEventDetails(event) {
+                $('.user_id', $eventViewer).text(event.user_id);
+                $('.name', $eventViewer).text(event.name);
+                $('.ip', $eventViewer).text(event.ip.length ? event.ip : event.ipv6);
+                $('.time', $eventViewer).text(event.time);
+                $('.event', $eventViewer).text(event.event);
+                $('.desc', $eventViewer).html(event.desc ? event.desc : '');
+            };
+
+            //append the layout to the current view container
+            $('.content').append($layout);
+            
+            //set up the student list
+            $eventList.datatable({
+                url: listUrl,
+                filter: true,
+                rowSelection: true,
+                model: [{
+                    id: 'user_id',
+                    label: __('User ID'),
+                    sortable: true,
+                    filterable: true
+                }, {
+                    id: 'name',
+                    label: __('User Name'),
+                    sortable: true,
+                    filterable: true
+                }, {
+                    id: 'event',
+                    label: __('Event'),
+                    sortable: true,
+                    filterable: true
+                }, {
+                    id: 'time',
+                    label: __('Time'),
+                    sortable: true,
+                    filterable: true
+                }],
+                
+                listeners: {
+                    /**
+                     * When a row is selected, we update the student viewer
+                     */
+                    selected: function selectRow(e, event) {
+                        updateEventDetails(event);
+
+                        //the 1st time it comes hidden
+                        $eventViewer.removeClass('hidden');
+                    },
+
+                    /**
+                     * Set pagination to start position when using filtering
+                     * @param e
+                     * @param options
+                     */
+                    filter: function filtered(e, options) {
+                        options.page = 1;
+                    }
+
+                }
+            }).on('create.datatable', function () {
+                // the page is now ready
+                $layout.trigger('dispatched');
+            });
         }
     };
 });
--- views/templates/TaoEventLog/log.tpl
+++ views/templates/TaoEventLog/log.tpl
@@ -1,27 +1,12 @@
+<?php
+use oat\tao\helpers\Template;
+
+?>
+
+<link rel="stylesheet" href="<?= Template::css('eventlog.css', 'taoEventLog') ?>"/>
+
 <header class="section-header flex-container-full">
-    <h2>Log watcher</h2>
+    <h2><?= __('Log watcher') ?></h2>
 </header>
 
-<form class="tao-scope">
-    <div class="grid-row">
-        <div class="col-4">
-            <input type="text" name="userId" placeholder="User identifier">
-        </div>
-        <div class="col-4">
-            <input type="text" placeholder="date-from">
-        </div>
-        <div class="col-sm-4">
-            <input type="text" placeholder="date-to">
-        </div>
-    </div>
-    <div class="grid-row">
-        <div class="col-sm-4">
-            <input type="text" placeholder="Term">
-        </div>
-    </div>
-    <div class="grid-row">
-        <div class="col-sm-4">
-            <button class="btn-info">Find</button>
-        </div>
-    </div>
-</form>
+<div class="content"></div>
